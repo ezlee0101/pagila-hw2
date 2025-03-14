ec9f9189bd322d0ffa52c1751a88659ba4c00fdf
ezlee0101 <easterbuun@gmail..com>
2025-03-13 21:47:09 -0700

    Initial commit

--- sql/05b.sql
+++ sql/05b.sql
@@ -2,4 +2,7 @@
  * This problem is the same as 05.sql,
  * but instead of using the NOT IN operator, you are to use a LEFT JOIN.
  */
-
+SELECT a.last_name, a.first_name
+FROM actor a
+LEFT JOIN customer c ON a.last_name || a.first_name = c.last_name || c.first_name
+WHERE c.customer_id IS NULL
